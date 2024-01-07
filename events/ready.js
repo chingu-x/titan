1d6de61187bbde6acca13943bedaa7c3e87c2ed3
Ruben Ellefsen <ellefsen88@gmail.com>
2024-01-07 20:12:20 +0100

    add on ready event. login, presence and ticket btn

---
+++ events/ready.js
@@ -0,0 +1,54 @@
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle, Events, EmbedBuilder } = require('discord.js');
+const openTicketChannel = '1193342042080817323'; // Open ticket channel ID
+
+module.exports = {
+    name: Events.ClientReady,
+    once: true,
+    execute: async (client) => {
+        console.log(`Ready! Logged in as ${client.user.tag}`);
+		
+		await client.user.setPresence({ 
+			activities: [{ 
+				name: 'DM for help' }], 
+				status: 'online' 
+			});
+
+        async function sendButtonMessage(client, channelId) {
+            // Get the channel
+            const channel = client.channels.cache.get(channelId);
+        
+            // Fetch and delete all messages in the channel
+            const fetched = await channel.messages.fetch({ limit: 100 });
+            fetched.forEach(async message => {
+                try {
+                    await message.delete();
+                } catch (error) {
+                    console.error(`Failed to delete message: ${error}`);
+                }
+            });
+        
+            // Create the initial button
+            const button = new ButtonBuilder()
+                .setCustomId('ticket_button')
+                .setLabel('📩 Create ticket')
+                .setStyle(ButtonStyle.Success);
+        
+            // Create an action row and add the button to it
+            const row = new ActionRowBuilder()
+                .addComponents(button);
+
+            // Create an embed
+            const welcomeEmbed = new EmbedBuilder()
+                .setTitle('Need any assistance?')
+                .setDescription('Welcome to our tickets channel. If you have any questions or inquiries, please click on the `Create ticket` button below to contact the staff!\n\n**After clicking on the button, you will receive a DM from our bot, Titan. When you send a DM to Titan, Titan will send that message to the Admins in a thread that is only visible by Chingu Admins. You will get a response from our Admins via DM in Titan.**')
+                .setColor('#6DE194')
+				.setThumbnail('https://imgur.com/EII19bn.png');
+        
+            // Send the button message to the channel with the embed
+            await channel.send({ embeds: [welcomeEmbed], components: [row] });
+        }
+
+        // Call the function inside the execute function
+        await sendButtonMessage(client, openTicketChannel);
+    }, 
+};
