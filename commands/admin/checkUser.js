e7e442047ca51d40ab3616dfc511113ad5b89383
Ruben Ellefsen <ellefsen88@gmail.com>
2024-01-14 20:56:06 +0100

    Add user and checkUser commands

--- commands/admin/checkUser.js
+++ commands/admin/checkUser.js
@@ -7,19 +7,36 @@ const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(process
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('user')
-        .setDescription('Provides information about you.'),
+        .setName('userCheck')
+        .setDescription('Provides information about a user.')
+        .addStringOption(option =>
+            option.setName('Discord ID')
+                .setDescription('The users Discord ID.')
+                .setRequired(true)),
     async execute(interaction) {
-		try {
+        // Check if the command is used in the specific channels
+        const allowedChannels = ['1195050183403262053', '1194954848089673728'];
+        if (!allowedChannels.includes(interaction.channelId)) {
+            return await interaction.reply({ content: 'You can only use this command in specific channels.', ephemeral: true });
+        }
+
+        // Get the Discord ID from the command argument
+        const discordId = interaction.options.getString('discordid');
+
+        // Fetch the user object using the Discord ID
+        const user = await interaction.client.users.fetch(discordId);
+        const username = user.username;
+
+        try {
             // Fetch the user's application from Airtable
             const applications = await base('Applications').select({
-                filterByFormula: `{Discord ID} = '${interaction.user.id}'`,
+                filterByFormula: `{Discord ID} = '${discordId}'`,
                 fields: ['Discord Name', 'Evaluation Status (from Solo Project Link)', 'Email']
             }).firstPage();
 
 			// Fetch the user's voyage signups from Airtable
 			const voyageSignups = await base('Voyage Signups').select({
-				filterByFormula: `{Discord ID} = '${interaction.user.id}'`,
+				filterByFormula: `{Discord ID} = '${discordId}'`,
                 fields: ['Discord Name', 'Email']
 			}).firstPage();
 
@@ -35,23 +52,21 @@ module.exports = {
 				const isDiscordNameMatch = applicationData['Discord Name'] === voyageSignupData['Discord Name'] ? 'Match <a:check:1196112072614887534>' : 'Mismatch :x:';
 				const isEmailMatch = applicationData['Email'] === voyageSignupData['Email'] ? 'Match <a:check:1196112072614887534>' : 'Mismatch :x:';
 				
-                // Get the evaluation status
                 let evaluationStatus = applicationData['Evaluation Status (from Solo Project Link)'];
-				if (Array.isArray(evaluationStatus)) {
-					if (evaluationStatus.includes('Passed')) {
-						evaluationStatus = 'Passed';
-					} else {
-						evaluationStatus = evaluationStatus[evaluationStatus.length - 1];
-					}
-				}
+                if (Array.isArray(evaluationStatus)) {
+                    if (evaluationStatus.includes('Passed')) {
+                        evaluationStatus = 'Passed';
+                    } else {
+                        evaluationStatus = evaluationStatus[evaluationStatus.length - 1];
+                    }
+                }
 				
-				const status = (applicationData['Discord Name'] === interaction.user.username) ? 'OK <a:check:1196112072614887534>' : 'Mismatch :x:';
+				const status = (applicationData['Discord Name'] === username) ? 'OK <a:check:1196112072614887534>' : 'Mismatch :x:';
                 const evaluationEmoji = (evaluationStatus.toLowerCase() === 'passed') ? '<a:check:1196112072614887534>' : ':x:';
 
-                // Create an embed message
                 const embed = new EmbedBuilder()
                     .setColor('#6DE194')
-                    .setTitle('User Information')
+                    .setTitle(`User Information`)
 					.setDescription("A check to see that your Discord account matches with your application, and that you've passed the Solo Project. If any of the fields show an :x:, please open a ticket in <#1194953897408737350> to resolve the issue.")
 					.addFields(
 						{ name: 'Email', value: applicationData['Email'] ? `${applicationData['Email']} <a:check:1196112072614887534>` : 'No email found :x:' },
@@ -66,9 +81,12 @@ module.exports = {
 					.setThumbnail('https://imgur.com/EII19bn.png');
 
                 // Reply with the user's information in an embed message
-                await interaction.reply({ embeds: [embed], ephemeral: true });
+                await interaction.reply({ 
+                    content: `User Information for <@${discordId}>`,
+                    embeds: [embed] 
+                });
             } else {
-                await interaction.reply({ content: `No additional information found for ${interaction.user.username}.`, ephemeral: true });
+                await interaction.reply({ content: `No additional information found for ${username}.`, ephemeral: true });
             }
         } catch (error) {
             console.error(error);
