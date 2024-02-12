41f0b9f36fe1f5691df2ce9204398cb8d9e0b70e
Ruben Ellefsen <ellefsen88@gmail.com>
2024-02-12 22:47:29 +0100

    Update broadcast.js
    now handles forum channels as well. sends the message the Welcome to your team channel thread

--- commands/admin/broadcast.js
+++ commands/admin/broadcast.js
@@ -5,7 +5,8 @@ module.exports = {
         .setName('broadcast')
         .setDescription('Send a message to all channels in a category.')
         .addStringOption(option => option.setName('category').setDescription('The ID of the category.').setRequired(true))
-        .addStringOption(option => option.setName('message').setDescription('The message to send.').setRequired(true)),
+        .addStringOption(option => option.setName('message').setDescription('The message to send.').setRequired(true))
+        .addStringOption(option => option.setName('header').setDescription('An optional header to display above the embed.').setRequired(false)),
     async execute(interaction) {
         try {
             // Check if the sender has the 'ADMINISTRATOR' permission
@@ -13,6 +14,7 @@ module.exports = {
                 // Extract the category ID and message from the options
                 const categoryId = interaction.options.getString('category');
                 const message = interaction.options.getString('message');
+                const header = interaction.options.getString('header');
 
                 // Create an embed with the message
                 const embed = new EmbedBuilder()
@@ -31,9 +33,26 @@ module.exports = {
                     const channels = interaction.guild.channels.cache.filter(channel => channel.parentId === category.id);
 
                     // Send the embed to all channels in the category
-                    channels.each(channel => {
+                    channels.each(async channel => {
                         if (channel.type === 0) {
-                            channel.send({ embeds: [embed] });
+                            if (header) {
+                                await channel.send({ content: header, embeds: [embed] });
+                            } else {
+                                await channel.send({ embeds: [embed] });
+                            }
+
+                        } else if (channel.type === 15) {
+                            channel.threads.fetch().then(fetchedThreads => {
+                                fetchedThreads.threads.each(thread => {
+                                    if (thread.name === 'Welcome to your team channel') {
+                                        if (header) {
+                                            thread.send({ content: header, embeds: [embed] });
+                                        } else {
+                                            thread.send({ embeds: [embed] });
+                                        }
+                                    }
+                                });
+                            }).catch(console.error);
                         }
                     });
 
