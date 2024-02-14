b3393b3c6a8bc3fa04d1a48cc2107f809d9bf44d
Cheryl M <webmaster@cherylli.com>
2024-02-14 19:35:02 +1100

    add voyage signup status to /user - merged with new code on dev

--- commands/utility/user.js
+++ commands/utility/user.js
@@ -6,21 +6,12 @@ require('dotenv').config();
 // Initialize Airtable
 const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(process.env.AIRTABLE_BASE);
 
-const getNextVoyageSignup = async (discordId) => {
-    return await base('Voyage Signups')
-        .select({
-            filterByFormula: `AND(Voyage = "V${nextVoyage}",{Discord ID}="${discordId}")`,
-            fields:['Commitment Form Completed', 'Tier'],
-            maxRecords: 3
-        }).firstPage()
-}
-
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('user')
         .setDescription('Provides information about you.'),
     async execute(interaction) {
-		try {
+        try {
             // Fetch the user's application from Airtable
             const applications = await base('Applications').select({
                 filterByFormula: `{Discord ID} = '${interaction.user.id}'`,
@@ -35,11 +26,11 @@ module.exports = {
             const application = applications[0];
             const applicationData = application.fields;
 
-			// Fetch the user's voyage signups from Airtable
-			const voyageSignups = await base('Voyage Signups').select({
-				filterByFormula: `{Discord ID} = '${interaction.user.id}'`,
-                fields: ['Discord Name', 'Email']
-			}).firstPage();
+            // Fetch the user's voyage signups from Airtable
+            const voyageSignups = await base('Voyage Signups').select({
+                filterByFormula: `AND(OR(Voyage = "V${nextVoyage}",Voyage = "V??"), {Discord ID} = '${interaction.user.id}')`,
+                fields: ['Discord Name', 'Email', 'Commitment Form Completed', 'Tier']
+            }).firstPage();
 
             let voyageSignupData = null;
             if (voyageSignups.length > 0) {
@@ -47,11 +38,20 @@ module.exports = {
                 voyageSignupData = voyageSignup.fields;
             }
 
-			// Fetch the user's solo project from Airtable
-			const soloProjects = await base('Solo Projects').select({
-				filterByFormula: `{Discord ID} = '${interaction.user.id}'`,
+            // User signup for next voyage
+            const nextVoyageSignupText = voyageSignups.length!==0?
+                `Yes (${voyageSignups[0].fields['Tier'].slice(0,6)}) <a:check:1196112072614887534> `:
+                'No :x: https://forms.gle/DajSfXQCX4qbMAu8A'
+            const commitmentFormText = voyageSignups.length===0? 'N/A':
+                voyageSignups[0]?.fields['Commitment Form Completed'] === 'Yes'?
+                    'Yes <a:check:1196112072614887534>':
+                    'No :x: https://forms.gle/p5bhpoKFVBatQhnCA'
+
+            // Fetch the user's solo project from Airtable
+            const soloProjects = await base('Solo Projects').select({
+                filterByFormula: `{Discord ID} = '${interaction.user.id}'`,
                 fields: ['Discord Name', 'Email', 'Tier', 'GitHub ID']
-			}).firstPage();
+            }).firstPage();
 
             let soloProjectData = null;
             if (soloProjects.length > 0) {
@@ -59,85 +59,50 @@ module.exports = {
                 soloProjectData = soloProject.fields;
             }
 
+            // Check if the Discord name and email match in both tables
+            const isDiscordNameMatch = applicationData && voyageSignupData && applicationData['Discord Name'] === voyageSignupData['Discord Name'] ? 'Match <a:check:1196112072614887534>' : (applicationData && voyageSignupData ? 'Mismatch :x:' : 'N/A');
+            const isEmailMatch = applicationData && voyageSignupData && applicationData['Email'] === voyageSignupData['Email'] ? 'Match <a:check:1196112072614887534>' : (applicationData && voyageSignupData ? 'Mismatch :x:' : 'N/A');
 
-
-            // User signup for next voyage
-            const nextVoyageSignup = await getNextVoyageSignup(interaction.user.id)
-            const nextVoyageSignupText = nextVoyageSignup.length!==0?
-                `Yes (${nextVoyageSignup[0].fields['Tier'].slice(0,6)}) <a:check:1196112072614887534> `:
-                'No :x: https://forms.gle/DajSfXQCX4qbMAu8A'
-            const commitmentFormText = nextVoyageSignup.length===0? 'N/A':
-                nextVoyageSignup[0]?.fields['Commitment Form Completed'] === 'Yes'?
-            'Yes <a:check:1196112072614887534>':
-            'No :x: https://forms.gle/p5bhpoKFVBatQhnCA'
-
-
-            // Check if the user's record exists
-            if (applications.length > 0) {
-                const application = applications[0];
-                const applicationData = application.fields;
-
-                let voyageSignupData;
-                if (voyageSignups.length > 0) {
-                    const voyageSignup = voyageSignups[0];
-                    voyageSignupData = voyageSignup.fields;
+            // Get the evaluation status
+            let evaluationStatus = applicationData['Evaluation Status (from Solo Project Link)'];
+            if (Array.isArray(evaluationStatus)) {
+                if (evaluationStatus.includes('Passed')) {
+                    evaluationStatus = 'Passed';
+                } else {
+                    evaluationStatus = evaluationStatus[evaluationStatus.length - 1];
                 }
+            }
 
-                // Check if the Discord name and email match in both tables
-				const isDiscordNameMatch = applicationData && voyageSignupData && applicationData['Discord Name'] === voyageSignupData['Discord Name'] ? 'Match <a:check:1196112072614887534>' : (applicationData && voyageSignupData ? 'Mismatch :x:' : 'N/A');
-                const isEmailMatch = applicationData && voyageSignupData && applicationData['Email'] === voyageSignupData['Email'] ? 'Match <a:check:1196112072614887534>' : (applicationData && voyageSignupData ? 'Mismatch :x:' : 'N/A');
-				
-                // Get the evaluation status
-                let evaluationStatus = applicationData['Evaluation Status (from Solo Project Link)'];
-				if (Array.isArray(evaluationStatus)) {
-					if (evaluationStatus.includes('Passed')) {
-						evaluationStatus = 'Passed';
-					} else {
-						evaluationStatus = evaluationStatus[evaluationStatus.length - 1];
-					}
-				}
-				
-				const status = (applicationData['Discord Name'] === interaction.user.username) ? 'OK <a:check:1196112072614887534>' : 'Mismatch :x:';
-                const evaluationEmoji = (evaluationStatus.toLowerCase() === 'passed') ? '<a:check:1196112072614887534>' : ':x:';
+            const status = (applicationData['Discord Name'] === interaction.user.username) ? 'OK <a:check:1196112072614887534>' : 'Mismatch :x:';
+            const evaluationEmoji = (evaluationStatus.toLowerCase() === 'passed') ? '<a:check:1196112072614887534>' : ':x:';
 
-                // Create an embed message
-                const embed = new EmbedBuilder()
-                    .setColor('#6DE194')
-                    .setTitle('User Information')
-                    .setDescription("A check to see that your Discord account matches with your application, and that you've passed the Solo Project. If any of the fields show an :x:, please open a ticket in <#1194953897408737350> to resolve the issue.")
-                    .addFields(
-                        { name: 'Email', value: applicationData && applicationData['Email'] ? `${applicationData['Email']} <a:check:1196112072614887534>` : 'No email found :x:', inline: true },
-                        { name: 'Github ID', value: soloProjectData && soloProjectData['GitHub ID'] ? `${soloProjectData['GitHub ID']}` : 'N/A', inline : true},
-                        { name: '\u200B', value: '\u200B' },
-                        { name: 'Discord account match', value: status ? status : 'N/A', inline: true },
-                        { name: 'Evaluation Status', value: evaluationStatus ? `${evaluationStatus} ${evaluationEmoji}` : 'N/A', inline: true },
-                        { name: 'Solo Project Tier', value: soloProjectData && soloProjectData['Tier'] ? `Tier ${soloProjectData['Tier'][5]}` : 'N/A', inline : true},
-                        { name: '\u200B', value: '\u200B' },
-                    )
-                    .addFields(
-                        { name: 'Voyage Email Match', value: isEmailMatch ? isEmailMatch : 'N/A', inline: true },
-                        { name: 'Voyage Discord Match', value: isDiscordNameMatch ? isDiscordNameMatch : 'N/A', inline: true },
-                    )
-					.setDescription("A check to see that your Discord account matches with your application, and that you've passed the Solo Project. If any of the fields show an :x:, please open a ticket in <#1194953897408737350> to resolve the issue.")
-					.addFields(
-						{ name: 'Email', value: applicationData['Email'] ? `${applicationData['Email']} <a:check:1196112072614887534>` : 'No email found :x:' },
-						{ name: 'Discord account match', value: status, inline: true },
-						{ name: 'Evaluation Status', value: `${evaluationStatus} ${evaluationEmoji}`, inline: true },
-						{ name: '\u200B', value: '\u200B' },
-					)
-					.addFields(
-						{ name: 'Voyage Email Match', value: isEmailMatch, inline: true },
-						{ name: 'Voyage Discord Match', value: isDiscordNameMatch, inline: true },
-                        { name: '\u200B', value: '\u200B' },
-                    )
-                    .addFields(
-                        { name: `Signed up for V${nextVoyage}?`, value: nextVoyageSignupText, inline:true},
-                        { name: `Commitment Form for V${nextVoyage}?`, value: commitmentFormText, inline:true}
-                    )
-					.setThumbnail('https://imgur.com/EII19bn.png');
+            // Create an embed message
+            const embed = new EmbedBuilder()
+                .setColor('#6DE194')
+                .setTitle('User Information')
+                .setDescription("A check to see that your Discord account matches with your application, and that you've passed the Solo Project. If any of the fields show an :x:, please open a ticket in <#1194953897408737350> to resolve the issue.")
+                .addFields(
+                    { name: 'Email', value: applicationData && applicationData['Email'] ? `${applicationData['Email']} <a:check:1196112072614887534>` : 'No email found :x:', inline: true },
+                    { name: 'Github ID', value: soloProjectData && soloProjectData['GitHub ID'] ? `${soloProjectData['GitHub ID']}` : 'N/A', inline : true},
+                    { name: '\u200B', value: '\u200B' },
+                    { name: 'Discord account match', value: status ? status : 'N/A', inline: true },
+                    { name: 'Evaluation Status', value: evaluationStatus ? `${evaluationStatus} ${evaluationEmoji}` : 'N/A', inline: true },
+                    { name: 'Solo Project Tier', value: soloProjectData && soloProjectData['Tier'] ? `Tier ${soloProjectData['Tier'][5]}` : 'N/A', inline : true},
+                    { name: '\u200B', value: '\u200B' },
+                )
+                .addFields(
+                    { name: 'Voyage Email Match', value: isEmailMatch ? isEmailMatch : 'N/A', inline: true },
+                    { name: 'Voyage Discord Match', value: isDiscordNameMatch ? isDiscordNameMatch : 'N/A', inline: true },
+                    {name: '\u200B', value: '\u200B'},
+                )
+                .addFields(
+                    {name: `Signed up for V${nextVoyage}?`, value: nextVoyageSignupText, inline: true},
+                    {name: `Commitment Form for V${nextVoyage}?`, value: commitmentFormText, inline: true}
+                )
+                .setThumbnail('https://imgur.com/EII19bn.png');
 
-                // Reply with the user's information in an embed message
-                await interaction.reply({ embeds: [embed], ephemeral: true });
+            // Reply with the user's information in an embed message
+            await interaction.reply({ embeds: [embed], ephemeral: true });
 
         } catch (error) {
             console.error(error);
