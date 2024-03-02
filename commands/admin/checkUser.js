559b225337cafb4b856f8c06af198b603132428b
Ruben Ellefsen <ellefsen88@gmail.com>
2024-03-02 08:59:08 +0100

    update checkUser.js to follow user.js

--- commands/admin/checkUser.js
+++ commands/admin/checkUser.js
@@ -46,7 +46,7 @@ module.exports = {
             // Fetch the user's voyage signups from Airtable
             const voyageSignups = await base('Voyage Signups').select({
                 filterByFormula: `AND(OR(Voyage = "V${nextVoyage}",Voyage = "V??"), {Discord ID} = '${discordId}')`,
-                fields: ['Discord Name', 'Email', 'Commitment Form Completed', 'Tier']
+                fields: ['Discord Name', 'Email', 'Commitment Form Completed', 'Tier', 'Voyage']
             }).firstPage();
 
             let voyageSignupData = null;
@@ -57,6 +57,7 @@ module.exports = {
 
             // User signup for next voyage
             const nextVoyageSignupText = voyageSignups.length!==0?
+                voyageSignupData["Voyage"] === "V??"?`Pending`:
                 `Yes (${voyageSignups[0].fields['Tier'].slice(0,6)}) <a:check:1196112072614887534> `:
                 'No :x:'
             const commitmentFormText = voyageSignups.length===0? 'N/A':
