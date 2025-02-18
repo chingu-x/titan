b3881dbb029506e19c1d4e9dcf9d88bf1596a3ec
Ruben Ellefsen <ellefsen88@gmail.com>
2025-02-18 21:21:29 +0100

    add dynamic fetching on the duplicate command

--- utils/constants.js
+++ utils/constants.js
@@ -1,3 +1,16 @@
-const nextVoyage = 51
+const { getCurrentAndNextVoyage } = require('../commands/utility/user.js');
 
-module.exports = {nextVoyage}
+async function getNextVoyage() {
+    const { nextVoyage } = await getCurrentAndNextVoyage();
+    return nextVoyage;
+}
+
+// Fetch the next voyage number and export it
+let nextVoyageNumber;
+getNextVoyage().then(number => {
+    nextVoyageNumber = number;
+}).catch(error => {
+    console.error('Error fetching next voyage:', error);
+});
+
+module.exports = { getNextVoyage, nextVoyageNumber };
