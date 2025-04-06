10f32931c91b89b433d773fde399e78e101a2714
Ruben Ellefsen <ellefsen88@gmail.com>
2025-04-06 18:20:46 +0200

    feat: buttons
    Add buttons for signup channel

--- handlers/buttons.js
+++ handlers/buttons.js
@@ -1,5 +1,33 @@
 const { ButtonBuilder, ButtonStyle } = require('discord.js');
 
+function createApplicationButton() {
+    return new ButtonBuilder()
+    .setCustomId('application_button')
+    .setLabel('📝 Application')
+    .setStyle(ButtonStyle.Primary);
+}
+
+function createSoloProjectButton() {
+    return new ButtonBuilder()
+    .setCustomId('solo_project_button')
+    .setLabel('💻 Submit Solo Project')
+    .setStyle(ButtonStyle.Primary);
+}
+
+function createVoyageSignupButton() {
+    return new ButtonBuilder()
+    .setCustomId('voyage_signup_button')
+    .setLabel('🚀 Voyage Signup')
+    .setStyle(ButtonStyle.Primary);
+}
+
+function createCommitmentButton() {
+    return new ButtonBuilder()
+    .setCustomId('commitment_button')
+    .setLabel('📝 Commitment')
+    .setStyle(ButtonStyle.Primary);
+}
+
 function createTicketButton() {
     return new ButtonBuilder()
     .setCustomId('ticket_button')
@@ -7,4 +35,10 @@ function createTicketButton() {
     .setStyle(ButtonStyle.Success);
 }
 
-module.exports = { createTicketButton };
+module.exports = { 
+    createTicketButton,
+    createApplicationButton,
+    createSoloProjectButton,
+    createVoyageSignupButton,
+    createCommitmentButton,
+};
