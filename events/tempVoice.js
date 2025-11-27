07b37dfe9528a09ac5c70af8a7e42be0c3747572
Cheryl Murphy <webmaster@cherylli.com>
2025-11-27 14:16:37 +1100

    add error handling

--- events/tempVoice.js
+++ events/tempVoice.js
@@ -72,13 +72,18 @@ async function createMenu(client, categoryIds, channelId) {
 
     if (botMessage) {
         console.log('Bot message already exists in the create voice channel. Deleting old messages...');
-
+        
         if (messages.size > 0) {
             console.log("Deleting messages from previous create voice channel session...")
-            // await channel.bulkDelete(messages);
-            // Bulk delete doesn't work for messages older than 14 days
+            // Note: channel.bulkDelete doesn't work for messages older than 14 days
             for (const message of messages.values()) {
-                await message.delete();
+                try {
+                    await message.delete();
+                } catch (error) {
+                    channel.send(`[tempVoice] Failed to delete message ${message.id}. Check railway logs for more details.`)
+                    console.error(`Failed to delete message ${message.id}: ${error}`);
+                    return
+                }
             }
         }
     }
